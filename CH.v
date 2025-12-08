9f6666bf8cd63d6e4920f287e775cf4266e266be
ValeriaAlmodovar <valeria.almodovar@upr.edu>
2025-12-08 14:11:08 -0400

    Fixed valores del CH

--- CH.v
+++ CH.v
@@ -9,21 +9,21 @@ module CH (
 
 always @(*) begin
     case (cond)
-        4'b0000: BR_TAKEN = 1'b1;                         // BA - ALWAYS
-        4'b0001: BR_TAKEN = (Z_CC == 1'b0);               // BNE
-        4'b0010: BR_TAKEN = (Z_CC == 1'b1);               // BE
-        4'b0011: BR_TAKEN = (!Z_CC && (N_CC == V_CC));    // BG
-        4'b0100: BR_TAKEN = (Z_CC || (N_CC != V_CC));     // BLE
-        4'b0101: BR_TAKEN = (N_CC == V_CC);               // BGE
-        4'b0110: BR_TAKEN = (N_CC != V_CC);               // BL
-        4'b0111: BR_TAKEN = (C_CC == 1'b1);               // BGU
-        4'b1000: BR_TAKEN = (C_CC == 1'b0);               // BLEU
-        4'b1001: BR_TAKEN = (C_CC == 1'b0);               // BCC
-        4'b1010: BR_TAKEN = (C_CC == 1'b1);               // BCS
-        4'b1011: BR_TAKEN = (N_CC == 1'b0);               // BPOS
-        4'b1100: BR_TAKEN = (N_CC == 1'b1);               // BNEG
-        4'b1101: BR_TAKEN = (V_CC == 1'b0);               // BVC
-        4'b1110: BR_TAKEN = (V_CC == 1'b1);               // BVS
+        4'b1000: BR_TAKEN = 1'b1;                         // BA - ALWAYS
+        4'b1001: BR_TAKEN = (Z_CC == 1'b0);               // BNE
+        4'b0001: BR_TAKEN = (Z_CC == 1'b1);               // BE
+        4'b1010: BR_TAKEN = (!Z_CC && (N_CC == V_CC));    // BG
+        4'b0010: BR_TAKEN = (Z_CC || (N_CC != V_CC));     // BLE
+        4'b1011: BR_TAKEN = (N_CC == V_CC);               // BGE
+        4'b0011: BR_TAKEN = (N_CC != V_CC);               // BL
+        4'b1100: BR_TAKEN = (C_CC == 1'b1);               // BGU
+        4'b0100: BR_TAKEN = (C_CC == 1'b0);               // BLEU
+        4'b1101: BR_TAKEN = (C_CC == 1'b0);               // BCC
+        4'b0101: BR_TAKEN = (C_CC == 1'b1);               // BCS
+        4'b1110: BR_TAKEN = (N_CC == 1'b0);               // BPOS
+        4'b0110: BR_TAKEN = (N_CC == 1'b1);               // BNEG
+        4'b1111: BR_TAKEN = (V_CC == 1'b0);               // BVC
+        4'b0111: BR_TAKEN = (V_CC == 1'b1);               // BVS
         default: BR_TAKEN = 1'b0;
     endcase
 
